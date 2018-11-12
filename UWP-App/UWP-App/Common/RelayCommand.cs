26fe5d065dcb2e2cd52f3b729e10d4bc8e684a5c
SofusHN <sofushn@hotmail.dk>
2018-11-12 22:09:00 +0100

    Created a Working NavigationView
    Created a NavigationView (hamburgerMenu)

---
+++ UWP-App/UWP-App/Common/RelayCommand.cs
@@ -0,0 +1,124 @@
+﻿using System;
+using System.Windows.Input;
+
+namespace UWP_App.Common
+{
+    /// <summary>
+    /// A command whose sole purpose is to relay its functionality 
+    /// to other objects by invoking delegates. 
+    /// The default return value for the CanExecute method is 'true'.
+    /// <see cref="OnCanExecuteChanged"/> needs to be called whenever
+    /// <see cref="CanExecute"/> is expected to return a different value.
+    /// </summary>
+    public class RelayCommand : ICommand
+    {
+        private readonly Action _execute;
+        private readonly Func<bool> _canExecute;
+
+        /// <summary>
+        /// Initializes a new instance of the <see cref="RelayCommand"/> class.
+        /// </summary>
+        /// <param name="execute">The execution logic.</param>
+        public RelayCommand(Action execute) : this(execute, null) { }
+
+        /// <summary>
+        /// Initializes a new instance of the <see cref="RelayCommand"/> class.
+        /// </summary>
+        /// <param name="execute">The execution logic.</param>
+        /// <param name="canExecute">The execution status logic.</param>
+        public RelayCommand(Action execute, Func<bool> canExecute)
+        {
+            _execute = execute ?? throw new ArgumentNullException("execute");
+            _canExecute = canExecute;
+        }
+
+        /// <summary>
+        /// Raised when RaiseCanExecuteChanged is called.
+        /// </summary>
+        public event EventHandler CanExecuteChanged;
+
+        /// <summary>
+        /// Determines whether this <see cref="RelayCommand"/> can _execute in its current state.
+        /// </summary>
+        /// <param name="parameter">
+        /// Data used by the command. If the command does not require data to be passed, this object can be set to null.
+        /// </param>
+        /// <returns>true if this command can be executed; otherwise, false.</returns>
+        public bool CanExecute(object parameter) => _canExecute == null || _canExecute();
+
+        /// <summary>
+        /// Executes the <see cref="RelayCommand"/> on the current command target.
+        /// </summary>
+        /// <param name="parameter">
+        /// Data used by the command. If the command does not require data to be passed, this object can be set to null.
+        /// </param>
+        public void Execute(object parameter) => _execute();
+
+        /// <summary>
+        /// Method used to raise the <see cref="CanExecuteChanged"/> event
+        /// to indicate that the return value of the <see cref="CanExecute"/>
+        /// method has changed.
+        /// </summary>
+        public void OnCanExecuteChanged() => CanExecuteChanged?.Invoke(this, EventArgs.Empty);
+    }
+
+    /// <summary>
+    /// A command whose sole purpose is to relay its functionality
+    /// to other objects by invoking delegates.
+    /// The default return value for the CanExecute method is 'true'.
+    /// <see cref="OnCanExecuteChanged" /> needs to be called whenever
+    /// <see cref="CanExecute" /> is expected to return a different value.
+    /// </summary>
+    /// <typeparam name="T">The type of the data the command will handle.</typeparam>
+    public class RelayCommand<T> : ICommand
+    {
+        private readonly Action<T> _execute;
+        private readonly Func<bool> _canExecute;
+
+        /// <summary>
+        /// Initializes a new instance of the <see cref="RelayCommand{T}"/> class.
+        /// </summary>
+        /// <param name="execute">The execution logic.</param>
+        public RelayCommand(Action<T> execute) : this(execute, null) { }
+
+        /// <summary>
+        /// Initializes a new instance of the <see cref="RelayCommand{T}"/> class.
+        /// </summary>
+        /// <param name="execute">The execution logic.</param>
+        /// <param name="canExecute">The execution status logic.</param>
+        public RelayCommand(Action<T> execute, Func<bool> canExecute)
+        {
+            _execute = execute ?? throw new ArgumentNullException("execute");
+            _canExecute = canExecute;
+        }
+
+        /// <summary>
+        /// Raised when RaiseCanExecuteChanged is called.
+        /// </summary>
+        public event EventHandler CanExecuteChanged;
+
+        /// <summary>
+        /// Determines whether this <see cref="RelayCommand{T}"/> can _execute in its current state.
+        /// </summary>
+        /// <param name="parameter">
+        /// Data used by the command. If the command does not require data to be passed, this object can be set to null.
+        /// </param>
+        /// <returns>true if this command can be executed; otherwise, false.</returns>
+        public bool CanExecute(object parameter) => _canExecute == null || _canExecute();
+
+        /// <summary>
+        /// Executes the <see cref="RelayCommand{T}"/> on the current command target.
+        /// </summary>
+        /// <param name="parameter">
+        /// Data used by the command. If the command does not require data to be passed, this object can be set to null.
+        /// </param>
+        public void Execute(object parameter) => _execute((T)parameter);
+
+        /// <summary>
+        /// Method used to raise the <see cref="CanExecuteChanged"/> event
+        /// to indicate that the return value of the <see cref="CanExecute"/>
+        /// method has changed.
+        /// </summary>
+        public void OnCanExecuteChanged() => CanExecuteChanged?.Invoke(this, EventArgs.Empty);
+    }
+}
---
+++ UWP-App/UWP-App/ViewModel/ShellPageViewModel.cs
@@ -0,0 +1,142 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Input;
+using Windows.UI.Xaml.Controls;
+using UWP_App.Common;
+using Microsoft.Xaml.Interactivity;
+using UWP_App.View;
+
+namespace UWP_App.ViewModel
+{
+    public class ShellPageViewModel : ObservableBase
+    {
+        private NavigationViewItem _selectedNavigationViewItem;
+
+        public ObservableCollection<NavigationViewItemBase> PrimaryItems { get; set; }
+        public NavigationViewItem SelectedNavigationViewItem
+        {
+            get => _selectedNavigationViewItem;
+            set => SetProperty(ref _selectedNavigationViewItem, value);
+        }
+
+        public ICommand NavigationCommand { get => new RelayCommand<NavigationViewSelectionChangedEventArgs>(Navigate); }
+        public ICommand LoadedCommand { get => new RelayCommand(NaviagtionViewLoaded); }
+        public ICommand GoBackCommand { get => new RelayCommand(BackRequested); }
+
+        public ShellPageViewModel()
+        {
+            PrimaryItems = new ObservableCollection<NavigationViewItemBase>();
+            PopulateNavigationMenu();
+        }
+
+
+        /// <summary>
+        /// Populates the navigation menu (PrimaryItems)
+        /// </summary>
+        private void PopulateNavigationMenu()
+        {
+            // creates a NavigationLink
+            // implicit converts it to a NavigationViewItem
+            // adds the item to the PrimaryItems list
+            PrimaryItems.Add(new NavigationLink<MainPage>("Main Page", Symbol.Home));
+
+            // adds a seperator between the MainPage and the header
+            PrimaryItems.Add(new NavigationViewItemSeparator());
+            // adds a NavigationViewHeader with som string content to the PrimaryItems list
+            PrimaryItems.Add(new NavigationViewItemHeader() { Content = "Other Pages" });
+
+            PrimaryItems.Add(new NavigationLink<LejlighedPage>("Lejlighed", Symbol.Home));
+            PrimaryItems.Add(new NavigationLink<TempTestPage>("Temp Test Page", Symbol.Pictures));
+
+        }
+
+        /// <summary>
+        /// Is called when NavigationView invokes its Loaded event (when the NavigationView is fully loaded)
+        /// </summary>
+        private void NaviagtionViewLoaded()
+        {
+            // Set the selected page to the first NavigationViewItem in PrimaryItems
+            // This invokes the Selection changed event in the NavigationView which runs the NavigationCommand
+            SelectedNavigationViewItem = (NavigationViewItem)PrimaryItems.FirstOrDefault(x => x.GetType() == typeof(NavigationViewItem));
+        }
+
+        private void Navigate(NavigationViewSelectionChangedEventArgs args)
+        {
+            // Check if the args is from the settings button
+            if (args.IsSettingsSelected)
+            {
+                throw new NotImplementedException("Feature not implemented!");
+            }
+            // Checks if the selected item is a NavigationViewItem
+            // If true convert the selected item to a NavigationViewItem
+            //      then converts the NavigationViewItems's tag property to a type and navigates to the page of that type
+            if (args.SelectedItem is NavigationViewItem navI)
+                NavigationService.Navigate(navI.Tag as Type);
+        }
+
+        /// <summary>
+        /// Is called when the BackRequested event in NavigationView is invoked
+        /// </summary>
+        private void BackRequested()
+        {
+            // Navigates one page back
+            NavigationService.GoBack();
+            // Gets type of current content in Frame
+            Type t = NavigationService.Frame.Content.GetType();
+            // Sets the selected navigation view item to the corosponding value from the PrimaryItem list
+            SelectedNavigationViewItem = PrimaryItems.FirstOrDefault(x => (Type)x.Tag == t) as NavigationViewItem;
+        }
+    }
+
+
+    /// <summary>
+    /// A class which contains a page, lable and symbol
+    /// It is a light version of NavigationViewItem
+    /// </summary>
+    /// <typeparam name="T">The page type the NavigationLink points to</typeparam>
+    public class NavigationLink<T>
+    {
+        public string Lable { get; set; }
+        public Type Page { get; set; }
+        public Symbol Symbol { get; set; }
+
+        public NavigationLink(string lable, Symbol symbol)
+        {
+            Lable = lable;
+            Page = typeof(T);
+            Symbol = symbol;
+        }
+
+        public static implicit operator NavigationViewItem(NavigationLink<T> link)
+        {
+            return new NavigationViewItem()
+            {
+                Icon = new SymbolIcon(link.Symbol),
+                Content = link.Lable,
+                Tag = link.Page
+            };
+        }
+
+        public static explicit operator NavigationLink<T>(NavigationViewItem item)
+        {
+            try
+            {
+                return new NavigationLink<T>(item.Content.ToString(), ((SymbolIcon) item.Icon).Symbol);
+            }
+            catch (Exception e)
+            {
+                throw e;
+            }
+            
+        }
+
+        public override string ToString()
+        {
+            return $"[Label: {Lable}; Page: {Page.Name}; Symbol: {Symbol}]";
+        }
+    }
+}
